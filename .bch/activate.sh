88540e94c7771624c66dc1afb9a4f84134dd6deb
Bryan Hann <nobody@nowhere>
2025-06-11 13:12:34 -0230

    Reduce dependancy on [bch:000:*] functions.

--- .bch/activate.sh
+++ .bch/activate.sh
@@ -8,8 +8,8 @@ export BCH_PYOPT__lbin=${r}/.bch/lbin
 export BCH_PYOPT__bin=${r}/.bch/bin
 export BCH_PYOPT__lib=${r}/.bch/lib
 
-bch:000:linkall ${r}/.bch/lbin
-
+source ${r}/.bch/init/fn.sh
 source ${r}/.bch/init/init.sh
+::lbin:: ${r}/.bch/lbin
 
 unset r
